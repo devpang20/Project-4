8c573cda80f37e1b55a06070b0ccc61ec67f02fd
sik2 <bickwang123@gmail.com>
2024-06-17 21:33:32 +0900

    삭제

--- 홍길동/src/page/RecodeDetail.jsx
+++ 홍길동/src/page/RecodeDetail.jsx
@@ -1,10 +1,20 @@
-import { Link, useParams } from 'react-router-dom'
+import { Link, Router, useNavigate, useParams } from 'react-router-dom'
 import { data } from '../data/dummy'
 
 function RecodeDetail() {
     const param = useParams()
+    const navigate = useNavigate()
 
     const detailData = data.find((row) => row.id == param.id)
+
+    const handelDel = (id) => {
+        fetch(`https://dummyjson.com/posts/${id}`, {
+            method: 'DELETE',
+        })
+            .then((res) => res.json())
+            .then(console.log)
+        navigate('/recode/list')
+    }
 
     return (
         <>
@@ -13,7 +23,7 @@ function RecodeDetail() {
             <div>내용 : {detailData.content}</div>
             <div>생성일 : {detailData.created}</div>
             <Link to={`/recode/${param.id}/edit`}>수정</Link>
-            <Link to="/recode/del">삭제</Link>
+            <button onClick={() => handelDel(detailData.id)}>삭제</button>
         </>
     )
 }
